b3b45e793c1163c429aaf4d46880e0089762b3f5
GabGabHub <gabibellol342@gmail.com>
2025-01-20 01:26:17 +0200

    We have an attendees list

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { QRCodeCanvas } from 'qrcode.react';
-import { delEvent } from '../api';
+import { delEvent, getAttendance } from '../api';
 import '../App.css';
 import ExportAttendance from './ExportAttendance';
 
 const EventList = ({ events, setEvents }) => {
+    const [attendeesMap, setAttendeesMap] = useState({});
+  
+    useEffect(() => {
+      const fetchAllAttendances = async () => {
+        const newAttendeesMap = {};
+        for (const event of events) {
+          try {
+            const response = await getAttendance(event.id);
+            newAttendeesMap[event.id] = response.data.map((att) => ({
+              participantName: att.Participant.name,
+              participantEmail: att.Participant.email,
+              timestamp: att.timestamp,
+            }));
+          } catch (error) {
+            console.error(`Error fetching attendance for event ${event.id}:`, error);
+          }
+        }
+        setAttendeesMap(newAttendeesMap);
+      };
+  
+      fetchAllAttendances();
+    }, [events, getAttendance]);
+    
+
+
   const handleDelete = (eventId) => {
     const confirmDelete = window.confirm('Are you sure you want to delete this event?');
     if (confirmDelete) {
@@ -32,6 +57,8 @@ const EventList = ({ events, setEvents }) => {
         const eventStartTime = new Date(event.startTime);
         const eventEndTime = new Date(event.endTime);
         const isOpen = currentTime >= eventStartTime && currentTime <= eventEndTime;
+        
+        const attendees = attendeesMap[event.id] || [];
 
         return (
           <div id="eventItems" key={event.id}>
@@ -43,7 +70,13 @@ const EventList = ({ events, setEvents }) => {
                 <p>Status: {isOpen ? 'OPEN' : 'CLOSED'}</p>
               </div>
               <QRCodeCanvas id="qrCode" value={`http://localhost:3000/Form/${event.accessCode}`} />
-           
+              <ul>
+                {attendees.map((participant, index) => (
+                    <li key={index}>
+                      {participant.participantName} ({participant.participantEmail}) - {participant.timestamp}
+                    </li>
+                  ))}
+              </ul>
             </div>
             <ExportAttendance event={event} />
             <button
